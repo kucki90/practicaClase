8ddc886215e0b9d2633a27c76d570eefda2d4515
kucki90 <c.kuckiewicz@hotmail.com>
2021-05-14 21:02:05 -0300

    cambie el machete al readme

--- git.js
+++
@@ -1,6 +0,0 @@
-git clone <https://link-con-nombre-del-repositorio>
-git branch <nombre-de-la-rama>
-git push <nombre-remoto> <nombre-rama></nombre-rama>
-
-git add . 
-git commit -m "mensaje de confirmación"
